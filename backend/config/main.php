76b7d5d8ee213ec9201967fa5db0e15d27907dfa
harlen-angke-mac <377304925@qq.com>
2017-08-31 11:54:40 +0800

    增加了主题动态配置

--- backend/config/main.php
+++ backend/config/main.php
@@ -31,7 +31,7 @@ return [
         'assetManager' => [
             'bundles' => [
                 'dmstr\web\AdminLteAsset' => [
-                    'skin' => 'skin-black',
+                    'skin' => 'skin-green-light',
                 ],
             ],
         ],
