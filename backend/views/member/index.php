0b2d4ddabad2a6612360c94390985169da7605c9
harlen-angke-mac <377304925@qq.com>
2017-09-08 20:47:39 +0800

    美化后台列表

--- backend/views/member/index.php
+++ backend/views/member/index.php
@@ -18,82 +18,91 @@ $this->params['breadcrumbs'][] = $this->title;
     <!--<p>
         <?/*= Html::a('Create Member', ['create'], ['class' => 'btn btn-success']) */?>
     </p>-->
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'pager'=>[
-            //'options'=>['class'=>'hidden']//关闭分页
-            'firstPageLabel'=>"首页",
-            'prevPageLabel'=>'上一页',
-            'nextPageLabel'=>'下一页',
-            'lastPageLabel'=>'尾页',
-        ],
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'name',
-            'sex',
-            'phone',
-            [
-                'attribute' => 'last_time',
-                'label'=>'创建时间',
-                'value'=>
-                    function($model){
-                        return  date('Y-m-d H:i:s',$model->last_time);   //主要通过此种方式实现
-                    },
-            ],
-            'identification',
-            'order_num',
-            'order_money',
-            [
-                'attribute' => 'status',
-                'value'=>function ($model,$key,$index,$column){
-                    return $model->status==1?'正常':'停封';
-                },
-        
-                'filter' => Html::activeDropDownList($searchModel,
-                    'status',['1'=>'正常','0'=>'停封'],
-                    ['prompt'=>'全部','style'=>'height:34px;']
-                )
-            ],
-            [
-                //动作列yii\grid\ActionColumn
-                //用于显示一些动作按钮，如每一行的更新、删除操作。
-                'class' => 'yii\grid\ActionColumn',
-                'header' => '操作',
-                'template' => '{update}{stop}{open}',//只需要展示删除和更新
-                'headerOptions' => ['width' => '100'],
-                'buttons' => [
-                    'update' => function ($url, $model, $key) {
-                        return Html::a(Html::tag('span', '修改电话', ['class' => "btn btn-xs btn-success"]), ['member/update', 'id'=>$model->id]). '&nbsp';
-                    },
-                    'stop' => function ($url, $model, $key) {
-                        if ($model->status==1){
-                            return Html::a('停封',
-                                    ['stop', 'id' => $model->id],
-                                    [
-                                        'class' => 'btn btn-success btn-xs',
-                                        'data' => ['confirm' => '你确定要停封该用户吗？', 'method' => 'post'],
-                                    ]
-                                ) . '&nbsp';
-                        }
-                       
-                    },
-                    'open' => function ($url, $model, $key) {
-                        if ($model->status==0){
-                            return Html::a('解封',
-                                ['open', 'id' => $model->id],
-                                [
-                                    'class' => 'btn btn-danger btn-xs',
-                                    'data' => ['confirm' => '你确定要解封该用户吗？', 'method' => 'post'],
-                                ]
-                            );
-                        }
-                        
-                    },
-                ],
-            ],
-        ],
-    ]);
-    ?>
+    <div class="box box-success">
+        <div class="box-header with-border">
+            <h3 class="box-title"></h3>
+            <div class="box-body">
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'pager'=>[
+                        //'options'=>['class'=>'hidden']//关闭分页
+                        'firstPageLabel'=>"首页",
+                        'prevPageLabel'=>'上一页',
+                        'nextPageLabel'=>'下一页',
+                        'lastPageLabel'=>'尾页',
+                    ],
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        'name',
+                        'sex',
+                        'phone',
+                        [
+                            'attribute' => 'last_time',
+                            'label'=>'创建时间',
+                            'value'=>
+                                function($model){
+                                    return  date('Y-m-d H:i:s',$model->last_time);   //主要通过此种方式实现
+                                },
+                        ],
+                        'identification',
+                        'order_num',
+                        'order_money',
+                        [
+                            'attribute' => 'status',
+                            'value'=>function ($model,$key,$index,$column){
+                                return $model->status==1?'正常':'停封';
+                            },
+
+                            'filter' => Html::activeDropDownList($searchModel,
+                                'status',['1'=>'正常','0'=>'停封'],
+                                ['prompt'=>'全部','style'=>'height:34px;']
+                            )
+                        ],
+                        [
+                            //动作列yii\grid\ActionColumn
+                            //用于显示一些动作按钮，如每一行的更新、删除操作。
+                            'class' => 'yii\grid\ActionColumn',
+                            'header' => '操作',
+                            'template' => '{update}{stop}{open}',//只需要展示删除和更新
+                            'headerOptions' => ['width' => '100'],
+                            'buttons' => [
+                                'update' => function ($url, $model, $key) {
+                                    return Html::a(Html::tag('span', '修改电话', ['class' => "btn btn-xs btn-success"]), ['member/update', 'id'=>$model->id]). '&nbsp';
+                                },
+                                'stop' => function ($url, $model, $key) {
+                                    if ($model->status==1){
+                                        return Html::a('停封',
+                                                ['stop', 'id' => $model->id],
+                                                [
+                                                    'class' => 'btn btn-success btn-xs',
+                                                    'data' => ['confirm' => '你确定要停封该用户吗？', 'method' => 'post'],
+                                                ]
+                                            ) . '&nbsp';
+                                    }
+
+                                },
+                                'open' => function ($url, $model, $key) {
+                                    if ($model->status==0){
+                                        return Html::a('解封',
+                                            ['open', 'id' => $model->id],
+                                            [
+                                                'class' => 'btn btn-danger btn-xs',
+                                                'data' => ['confirm' => '你确定要解封该用户吗？', 'method' => 'post'],
+                                            ]
+                                        );
+                                    }
+
+                                },
+                            ],
+                        ],
+                    ],
+                ]); ?>
+            </div>
+            <div class="box-footer clearfix">
+
+            </div>
+        </div>
+    </div>
 </div>
 
--- backend/views/salesman/index.php
+++ backend/views/salesman/index.php
@@ -18,68 +18,78 @@ use yii\grid\GridView;
     <p>
         <?= Html::a('添加业务员', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-    
-    <?= GridView::widget([
-        'tableOptions' => ['class' => 'table table-hover'],
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'layout'=> '{items}<div class="text-right tooltip-demo">{pager}</div>',
-        'pager'=>[
-            //'options'=>['class'=>'hidden']//关闭分页
-            'firstPageLabel'=>"首页",
-            'prevPageLabel'=>'上一页',
-            'nextPageLabel'=>'下一页',
-            'lastPageLabel'=>'尾页',
-        ],
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'name',
-            'job_number',
-            'phone',
-            'bound_merchant',
-            [
-                'attribute' => 'created_at',
-                'label'=>'创建时间',
-                'value'=>
-                    function($model){
-                        return  date('Y-m-d H:i:s',$model->created_at);   //主要通过此种方式实现
-                    },
-                /*'filter'    => \kartik\datetime\DateTimePicker::widget([
-                    'model'         => $searchModel,
-                    'attribute'     => 'created_at',
-                    //'convertFormat' => true,
-                    'pluginOptions' => [
-                        'locale' => ['format' => 'Y-m-d H:i:s'],
+    <div class="box box-success">
+        <div class="box-header with-border">
+            <h3 class="box-title"></h3>
+            <div class="box-body">
+                <?= GridView::widget([
+                    'tableOptions' => ['class' => 'table table-hover table-striped'],
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'layout'=> '{items}<div class="text-right tooltip-demo">{pager}</div>',
+                    'pager'=>[
+                        //'options'=>['class'=>'hidden']//关闭分页
+                        'firstPageLabel'=>"首页",
+                        'prevPageLabel'=>'上一页',
+                        'nextPageLabel'=>'下一页',
+                        'lastPageLabel'=>'尾页',
                     ],
-                ]),*/
-            ],
-            [
-                //动作列yii\grid\ActionColumn
-                //用于显示一些动作按钮，如每一行的更新、删除操作。
-                'class' => 'yii\grid\ActionColumn',
-                'header' => '操作',
-                'template' => '{view}{update}{delete}',//只需要展示删除和更新
-                'headerOptions' => ['width' => '240'],
-                'buttons' => [
-                    'view' => function ($url, $model, $key) {
-                        return Html::a(Html::tag('span', '查看详情', ['class' => "btn btn-xs btn-primary"]), ['/salesman/view', 'id'=>$model->id]).'&nbsp';
-                    },
-                    'update' => function ($url, $model, $key) {
-                        return Html::a(Html::tag('span', '修改', ['class' => "btn btn-xs btn-success"]), ['/salesman/update', 'id'=>$model->id]).'&nbsp';
-                    },
-                    'delete' => function($url, $model, $key){
-                        return Html::a('删除',
-                            ['delete', 'id' => $key],
-                            [
-                                'class' => 'btn btn-danger btn-xs',
-                                'data' => ['confirm' => '你确定要删除文章吗？','method' => 'post']
-                            ]
-                        ).'&nbsp';
-                    },
-                    
-                ],
-            ],
-        ],
-    ]); ?>
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        'name',
+                        'job_number',
+                        'phone',
+                        'bound_merchant',
+                        [
+                            'attribute' => 'created_at',
+                            'label'=>'创建时间',
+                            'value'=>
+                                function($model){
+                                    return  date('Y-m-d H:i:s',$model->created_at);   //主要通过此种方式实现
+                                },
+                            /*'filter'    => \kartik\datetime\DateTimePicker::widget([
+                                'model'         => $searchModel,
+                                'attribute'     => 'created_at',
+                                //'convertFormat' => true,
+                                'pluginOptions' => [
+                                    'locale' => ['format' => 'Y-m-d H:i:s'],
+                                ],
+                            ]),*/
+                        ],
+                        [
+                            //动作列yii\grid\ActionColumn
+                            //用于显示一些动作按钮，如每一行的更新、删除操作。
+                            'class' => 'yii\grid\ActionColumn',
+                            'header' => '操作',
+                            'template' => '{view}{update}{delete}',//只需要展示删除和更新
+                            'headerOptions' => ['width' => '240'],
+                            'buttons' => [
+                                'view' => function ($url, $model, $key) {
+                                    return Html::a(Html::tag('span', '查看详情', ['class' => "btn btn-xs btn-primary"]), ['/salesman/view', 'id'=>$model->id]).'&nbsp';
+                                },
+                                'update' => function ($url, $model, $key) {
+                                    return Html::a(Html::tag('span', '修改', ['class' => "btn btn-xs btn-success"]), ['/salesman/update', 'id'=>$model->id]).'&nbsp';
+                                },
+                                'delete' => function($url, $model, $key){
+                                    return Html::a('删除',
+                                            ['delete', 'id' => $key],
+                                            [
+                                                'class' => 'btn btn-danger btn-xs',
+                                                'data' => ['confirm' => '你确定要删除文章吗？','method' => 'post']
+                                            ]
+                                        ).'&nbsp';
+                                },
+
+                            ],
+                        ],
+                    ],
+                ]); ?>
+            </div>
+            <div class="box-footer clearfix">
+
+            </div>
+        </div>
+    </div>
+
     
 </div>
